ef05536ab624bd05852160e376b2100f8f8265d4
RullV2 <136607912+RullV2@users.noreply.github.com>
2023-06-17 12:43:28 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -45,8 +45,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	
 	
 	# R11.1 Settings
-	export FOX_VERSION="R11"
-	export FOX_BUILD_TYPE="Stable"
+	#export FOX_VERSION="R11"
+	#export FOX_BUILD_TYPE="Stable"
 	export OF_MAINTAINER="MASRULL"
 	
 
